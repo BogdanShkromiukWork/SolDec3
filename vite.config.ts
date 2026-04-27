6b6204a3913d3ca60b3dcdefdf92cf1072a3f23b
Bogdan Shkromiuk <bogdanshkromiukwork@gmail.com>
2026-04-27 14:33:49 -0700

    git_pages_6

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // Замени 'soldec3' на точное название твоего репозитория на GitHub
-  base: '/soldec3/', 
+  base: '/SolDec3/', 
   build: {
     outDir: 'dist',
   },
